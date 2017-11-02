8d3d28d06a71e1a609a2b752764354852bbaadb7
KakaliKobal <demonaddicted@outlook.com>
2017-11-01 18:20:03 -0600

    working on name and id's

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,29 +1,93 @@
 var config = {
     apiKey: "AIzaSyBC5G9E8Gqidb41VIT_1wz_wEq0mGNy1s4",
     authDomain: "rps-multiplayer-5fec0.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-5fec0.firebaseio.com"
+    databaseURL: "https://rps-multiplayer-5fec0.firebaseio.com",
     projectId: "rps-multiplayer-5fec0",
     storageBucket: "rps-multiplayer-5fec0.appspot.com",
     messagingSenderId: "413398297945"
 };
 firebase.initializeApp(config);
 
+
+var chatLog = [];
 var database = firebase.database();
 
 
-var game = {
-	players : [
-	],
-	chat: []
-}
+if (localStorage.name) {
+	whoami = localStorage.name;
+	database.ref("users/" + whoami).once('value', function(snapshot) {
+		if (snapshot.val() == null) {
+			localStorage.removeItem(name);
+		} else {
+			$('#set-name').hide();
+			$('#type-chat').removeAttr("disabled");
+		}
+	})
+} 
 
-game['players'][0]['name'] = "Player 1"
 
-$("#add-name").on("click", function(event) {
+$('#add-name').on("click", function(event) {
 	event.preventDefault();
 	var name = $("#fill-name").val().trim();
-  	name = name.charAt(0).toUpperCase() + name.slice(1);
+  	whoami = name.charAt(0).toUpperCase() + name.slice(1);
+
+  	database.ref('users/' + whoami).set({
+  		wins: 0,
+  		choice: ""
+  	});
+  	$('#set-name').hide();
+  	localStorage.name = whoami;
   	
-})
+	$('#type-chat').removeAttr("disabled");
+});
+
+// Firebase watcher + initial loader HINT: .on("value")
+database.ref().on("value", function(snapshot) {
+	if (!(snapshot.val().chatlog)) {chatLog = [];}
+	// Log everything that's coming out of snapshot
+
+	snapshot.val().users
+	console.log(snapshot.val());
+
+	updateChatLog(chatLog);
+
+	
+
+// Handle the errors
+}, function(errorObject) {
+	console.log("Errors handled: " + errorObject.code);
+});
 
 
+
+function updateChatLog(chatlog) {
+	if (!chatlog) return
+	box = $('#chat-area')
+	box.empty();
+	chatText = "";
+	for (i = 0; i < chatlog.length; i++) {
+		chatText += chatlog[i] + "\n"
+	}
+	box.val(chatText);
+	bottom = box.prop('scrollHeight') - box.height();
+	box.scrollTop(bottom);
+
+}
+
+function sendMessage() {
+	var message = $('#type-chat').val().trim();
+	if (message == '') return
+
+	$('#type-chat').val("");
+
+	message = whoami + ": " + message;
+	
+	chatLog.push(message);
+
+	database.ref('chatlog').set(chatLog);
+}
+
+
+$( document ).ready(function() {
+	$('#add-chat').on("click", sendMessage);
+});
